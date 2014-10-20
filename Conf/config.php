bcc542090729a7ca726b612bc4d5279463ee66c9
yachtxiao <fuck@fackyou.org>
2014-10-20 09:08:44 +0800

    还差一步就完成

--- Conf/config.php
+++ Conf/config.php
@@ -1,5 +1,33 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'authkey' => 'fuckhack',
+	'LANG_AUTO_DETECT' => FALSE, //关闭语言的自动检测，如果你是多语言可以开启
+    'LANG_SWITCH_ON' => TRUE, //开启语言包功能，这个必须开启
+    'DEFAULT_LANG' => 'zh-cn', //zh-cn文件夹名字 /lang/zh-cn/common.php
+	'URL_MODEL' => 0,
+	
+    /* 数据库设置 */
+    'DB_TYPE'               => 'mysql',     // 数据库类型
+    'DB_HOST'               => '112.124.46.98', // 服务器地址
+    'DB_NAME'               => 'checkin',          // 数据库名
+    'DB_USER'               => 'root',      // 用户名
+    'DB_PWD'                => 'Gjh19910726',          // 密码
+    'DB_PORT'               => '3306',        // 端口
+    'DB_PREFIX'             => '',    // 数据库表前缀
+    'DB_CHARSET'            => 'utf8',      // 数据库编码默认采用utf8
+	
+	/*远程公共文件设置*/
+	'S_JS' => 'http://static.lougacai.com/js/',
+	'S_CSS' => 'http://static.lougacai.com/style/',
+	'S_IMG' => 'http://static.lougacai.com/image/',
+	'S_ADMINSTYLE' => 'http://static.lougacai.com/admin/',
+	
+	/*又拍云空间账号*/
+	'IMGSPACE' => 'mobiledp',
+	'IMGUSER' => 'yachtxiao',
+	'IMGPWD' => 'guanjiahao',
+	'IMGURL' => 'http://img.lougacai.com',
+	
 );
 ?>
--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -2,6 +2,82 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
+		$time = time();
+		$record = D('checkin')->where('dateline = '.strtotime(date('Y-m-d',$time)))->find();
+		$status['morning'] = $record['morning']==0?'未打卡':"<span class='text-primary'><i class='glyphicon glyphicon-ok'></i>".date('H:i',$record['morning'])."</span>";
+		$status['noon'] = $record['noon']==0?'未打卡':"<span class='text-primary'><i class='glyphicon glyphicon-ok'></i>".date('H:i',$record['noon'])."</span>";
+		$status['night'] = $record['night']==0?'未打卡':"<span class='text-primary'><i class='glyphicon glyphicon-ok'></i>".date('H:i',$record['night'])."</span>";
+		if(($record['morning']==0)||($record['noon']==0)||($record['night']==0)){
+			$done = array('status'=>0,'date'=>date('Y年m月d日',$time));
+		}else{
+			$done = array('status'=>1,'date'=>date('Y年m月d日',$time));
+		}
+		$hour = date('H',time());
+		switch($hour){
+			case ((07<=$hour)&&($hour<11)):
+				if($record['morning']==0){
+					$return = 1;
+				}
+				break;
+			case ((11<=$hour)&&($hour<17)):
+				if($record['noon']==0){
+					$return = 1;
+				}
+				break;
+			case ((17<=$hour)&&($hour<23)):
+				if($record['night']==0){
+					$return = 1;
+				}
+				break;
+		}
+		$this->assign('intime',$return);
+		$this->assign('done',$done);
+		$this->assign('status',$status);
 		$this->display();
     }
+	
+	public function checkin(){
+		$time = time();
+		$hour = date('H',$time);
+		$return = 0;
+		switch($hour){
+			case ((07<=$hour)&&($hour<11)):
+				$return = $this->check(1);
+				break;
+			case ((11<=$hour)&&($hour<17)):
+				$return = $this->check(2);
+				break;
+			case ((17<=$hour)&&($hour<23)):
+				$return = $this->check(3);
+				break;
+		}
+		exit(json_encode($return));
+	}
+	
+	public function check($num){
+		$time = time();
+		$record = D('checkin')->where('dateline = '.strtotime(date('Y-m-d',$time)))->find();
+		if($num==1){
+			$data['morning'] = $time;
+		}
+		if($num==2){
+			$data['noon'] = $time;
+		}
+		if($num==3){
+			$data['night'] = $time;
+		}
+		if(!empty($record)){
+			$result = D('checkin')->where('dateline = '.strtotime(date('Y-m-d',$time)))->save($data);
+		}else{
+			$data['dateline'] = strtotime(date('Y-m-d',$time));
+			$result = D('checkin')->add($data);
+		}
+		if($result){
+			$return = array('status'=>1,'time'=>date('H:i',$time),'dkc'=>$num);
+		}else{
+			$return = array('status'=>0,'info'=>'打卡失败程序错误');
+		}
+		return $return;
+	}
+			
 }
