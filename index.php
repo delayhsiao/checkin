e9860f6f4f5a15a2110fc2839e51f1466aa9751b
yachtxiao <fuck@fackyou.org>
2014-10-20 09:15:15 +0800

    基础文件

---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+	/*define('site_domain','http://'.$_SERVER['HTTP_HOST']);
+	define('site_url',site_domain);
+	define('site_path',dirname(__FILE__));
+	define('APP_DEBUG',true);*/
+	require_once "./core/ThinkPHP.php";
+?>
